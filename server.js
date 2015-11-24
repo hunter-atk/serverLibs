1b470cd6888eaf3f44cf24f4d0fa7176e3ada5a8
Zubair Desai <zubair.desai@gmail.com>
2015-11-24 11:15:23 -0800

    routes

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var http = require('http');
+var gRouter = require('./gRouter');
 
 var server = http.createServer( function( request, response ) {
+
+  // grab req and res
+  console.log(request.url);
+  gRouter.getInfoFor( request, response )
   response.end( "bam: " + request.url );
+})
+
+gRouter.route("/", function( request, response ){
+  console.log("route bam");
 })
 
 var PORT = 4567
 server.listen( PORT, function() {
   console.log("listening on on:" + PORT );
 } )
+
+
+// request.method
+// request.statusCode
+// request.url
+// request.headers
+// request.headers: host
