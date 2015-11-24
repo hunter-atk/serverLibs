f8bb0ae03d8ccfe489ad1d9ed99d43f2985c01b1
Zubair Desai <zubair.desai@gmail.com>
2015-11-24 09:56:38 -0800

    http boiler

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-ECHO is on.
+var http = require('http');
+
+var server = http.createServer( function( request, response ) {
+  response.end( "bam: " + request.url );
+})
+
+var PORT = 4567
+server.listen( PORT, function() {
+  console.log("listening on on:" + PORT );
+} )
