80742979b6b1dfd27f7cb50425186df830843b12
Hunter Jefferson <hunterjefferson@Hunters-MacBook-Pro.local>
2018-03-21 10:49:56 -0500

    Test commit

--- server.js
+++ server.js
@@ -1,36 +1,37 @@
+// Do not import any other modules.
 var http = require('http');
 var fs = require('fs');
 
-// Get the server host from another student,
+// Get the sever host from another student,
 // this is where you will get your story from
 // they should have a sendstory route that sends their updated story
 
-var previousNode = 'serverlibs.herokuapp.com';
+// A working solution exists at this url
+// Once you are ready to test with a partner, replace this url
+// with your partners URL. The story at this default is very basic.
+var previousNode = 'https://fast-hollows-88598.herokuapp.com/';
 
 var server = http.createServer( function( request, response ) {
-
-  var route;
-  var url  = request.url;
+  // Every request to your server starts here.
 
   // check if the url has no path
   // If there is one, then grab the path, and ignore any query strings
-  if (url === "/") {
-    route = "root"
+  if (request.url === "/") {
+    routes['root'](request.url, request, response);
   } else {
-    route = url.split("?")[0].slice(1,url.length);
-  }
+    // Here, parse the request url to get the route
+    // then call the appropriate function.
 
-  // find the route (a.k.a path) from the routes object
-  if ( routes.hasOwnProperty(route) ) {
-    routes[route]( url , request, response );
   }
-
-})
+});
 
 // the routes object contains each route as a key.
 var routes = {
 
+  // root is done for you. :)
   root: function( url, request, response) {
+    // __dirname is handled by node. It is the directory
+    // of the server.js file.
     var filePath = __dirname + "/index.html";
     var stat = fs.statSync(filePath);
 
@@ -50,115 +51,52 @@ var routes = {
     });
   },
 
+  /**
+    Updates the story based on the query parameter in the url.
+  */
   updatestory: function( url, request, response) {
-    var query = url.split("?")[1]
+    // Fetch the passed in word from the url query parameters
 
-    if (query) {
-      addMadLib(query);
-      response.end("adding word to story: " + query)
-    } else {
-      response.end("no query sent");
-    }
+    // get the story file
+
+    // save the word into the first available placeholder
+    // those are {{ verb }}, {{ noun }} or {{ adjective }}
+
+    // save the story file
+
+    // send the updated version of the story to the browser
   },
 
+  /**
+    sends the current text of story.txt to whoever made
+    the request.
+  */
   sendstory: function( url, request, response ){
+    // read the story
 
-    var filePath = __dirname + "/story.txt";
-    var stat = fs.statSync(filePath);
-
-    fs.readFile(filePath, function (err, data) {
-      if (err) {
-        return console.error(err);
-      }
-
-      // specificy headers in the response
-      response.writeHead(200, {
-        'Content-Type': 'text/javascript',
-        'Content-Length': stat.size,
-      });
-
-      response.end(data);
-    });
+    // send the story
   },
 
+  /**
+    Fetch the story from your partners server by making
+    an HTTP request to their /sendstory route.
+
+    Once you have recived your partners story, replace
+    your own story.txt file with this data.
+  */
   getstory: function( url, request, response ){
 
-    var filePath = __dirname + "/story.txt"
-    var stat = fs.statSync(filePath);
+    // var filePath = __dirname + "/story.txt"
+    // var stat = fs.statSync(filePath);
 
-    var options = {
-         host: previousNode,
-         path: '/sendstory',
-      };
+    // Make an HTTP request to your partners server
+      // When that HTTP request has been processed, and
+      // you have the data, save that data into your story.txt
+      // file.
 
-    // Make a request to the server
-    var req = http.request(options, function(res){
-      var content = '';
-      console.log(res.headers);
-
-      // res is a stream, so we continously add data to the body string
-      res.on('data', function(data) {
-        content += data;
-      });
-
-      // once the response stream is finished
-      res.on('end', function() {
-      // Data received completely.
-        console.log("Received story: ", content);
-
-        response.writeHead(200, {
-           'Content-Type': 'text/javascript',
-           'Content-Length': stat.size
-        });
-
-        saveStory(content);
-        response.end(content);
-      });
-    });
-
-    req.end();
+      // Don't forget to end the request.
   }
-}
-
-// saves a story from a server with the getstory route
-function saveStory(story){
-  fs.writeFile('story.txt', story,  function(err) {
-    if (err) {
-       return console.error(err);
-    }
-    console.log("Data saved to story.txt successfully!");
-  });
-}
-
-// used in the updatestory route
-function addMadLib(queryWord) {
-  // get the current story
-  var story;
-
-  fs.readFile('story.txt', function (err, data) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("Reading story.txt:\n" + data.toString());
-    story = data.toString();
-
-    // update story with new word
-    addNewWord(story, queryWord)
-  });
-}
-
-// Used in addMadLib to add a new work from the string query to the story.txt
-function addNewWord(currentStory, newWord){
-  var placeholderText = /({{.*?}})/
-  updatedStory = currentStory.replace(placeholderText, newWord)
-
-  fs.writeFile('story.txt', updatedStory,  function(err) {
-    if (err) {
-       return console.error(err);
-    }
-    console.log("Data written successfully!");
-  });
-}
+};
 
 var PORT = process.env.PORT || 4567
 
